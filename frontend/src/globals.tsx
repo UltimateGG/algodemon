4c628def25ce5d1bf9703640b64af31b93d392d9
Gamer745 <clubblock1@gmail.com>
2022-09-16 00:21:48 -0500

    Add live key

--- frontend/src/globals.tsx
+++ frontend/src/globals.tsx
@@ -1,7 +1,9 @@
 export const PRICE = 29.99;
 export const IS_SALE = true;
 export const ORIG_PRICE = 149.99;
-export const STRIPE_PUBLIC_KEY = 'pk_test_51LiTBQJe97HcqNhtpmqJiqMNdmnkwwlEzJJhuKiHIezAzzZLsHLd7KVLw0HuKr37I6CZUSZ5yAfroO5DNBUlNjec00VnIaPoaI';
+export const STRIPE_PUBLIC_KEY = process.env.NODE_ENV === 'development'
+  ? 'pk_test_51LiTBQJe97HcqNhtpmqJiqMNdmnkwwlEzJJhuKiHIezAzzZLsHLd7KVLw0HuKr37I6CZUSZ5yAfroO5DNBUlNjec00VnIaPoaI'
+  : 'pk_live_51LiTBQJe97HcqNhtl3fwXIKh1yIVVwWdRRsuxb4dV68o9lbQNX044I35aBHbVVqt3eErpjpYmhBO1nepyqNtNkxI00P16SQ7OJ';
 
 export const AllCountries = [  
   {code: 'AF', name: 'Afghanistan'},
