dbace4813aca016c09e02688afba8926664883f3
UltimateGG <clubblock1@gmail.com>
2022-11-26 11:41:28 -0600

    Disable free trials

--- frontend/src/globals.tsx
+++ frontend/src/globals.tsx
@@ -4,7 +4,7 @@ export const DISCORD_URL = 'https://discord.gg/J4ENkbshTE';
 export const THEME_COLOR = '#6200EE';
 export const PRICE = 124.95;//149.99;
 export const AFFILIATE_PERCENT = 10.0;
-export const FREE_TRIALS_ACTIVE = true;
+export const FREE_TRIALS_ACTIVE = false;
 export const PAYPAL_CLIENT_ID = process.env.NODE_ENV === 'development'
   ? 'AXLSvOucX2MLY-m-dWu1BbCNF51FTJlKYqtkQuxPh93YPxj7iWxK874Cnpn7ddCFLaYJFR75xZO5Ed7H'
   : 'AffQ0kX6TJRQuZmK-n6E3CH1aJQuT3_x4xxtAFqTbLRhKFLZjFLQCdcmvN2qmvOuUNTAp72iu_QQVLPs';
