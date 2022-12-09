cc1a1ef44e48fe6ffe0d9803b26cafe6de86d10e
UltimateGG <clubblock1@gmail.com>
2022-12-09 13:09:58 -0600

    Change color scheme (temp test)

--- frontend/src/globals.tsx
+++ frontend/src/globals.tsx
@@ -1,7 +1,7 @@
 export const NAME = 'AlgoDemon';
 export const URL = 'https://algodemon.com';
 export const DISCORD_URL = 'https://discord.gg/J4ENkbshTE';
-export const THEME_COLOR = '#6200EE'; // 6200EE 02ff30
+export const THEME_COLOR = '#00df29'; // 6200EE 00df29
 export const PAYPAL_CLIENT_ID = process.env.NODE_ENV === 'development'
   ? 'AXLSvOucX2MLY-m-dWu1BbCNF51FTJlKYqtkQuxPh93YPxj7iWxK874Cnpn7ddCFLaYJFR75xZO5Ed7H'
   : 'AffQ0kX6TJRQuZmK-n6E3CH1aJQuT3_x4xxtAFqTbLRhKFLZjFLQCdcmvN2qmvOuUNTAp72iu_QQVLPs';
